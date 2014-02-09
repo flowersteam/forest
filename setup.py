bf2101dc3b85d2e60c79d59bf747f0d4d4b6ab36
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-02-09 21:31:46 +0100

    treecfg -> trees in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='trees',
       version='1.0a1',
       author='Fabien Benureau',
-      author_email='fabien.benureau+treecfg@gmail.com',
+      author_email='fabien.benureau+trees@gmail.com',
       license='not yet decided',
       packages=find_packages()
      )
