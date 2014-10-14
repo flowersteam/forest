3f5747d237624dc653186bdc1d122f190b6408d5
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-10-14 12:47:23 +0200

    1.0 version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='forest',
-      version='1.0.b1',
+      version='1.0',
       author='Fabien Benureau',
       author_email='fabien.benureau+forest@gmail.com',
       license='not yet decided',
