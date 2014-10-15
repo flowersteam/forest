ebd3f521c57dd476f86642b04aedb164a09f695c
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-10-15 02:29:45 +0200

    update setup files

--- setup.py
+++ setup.py
@@ -4,6 +4,16 @@ setup(name='forest',
       version='1.0',
       author='Fabien Benureau',
       author_email='fabien.benureau+forest@gmail.com',
-      license='not yet decided',
-      packages=['forest']
+      url='flowers.inria.fr',
+      maintainer='Fabien Benureau',
+      license='LGPLv3',
+      packages=['forest'],
+      description='A python hierarchical configuration structure for scientific experiments',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+      ]
      )
